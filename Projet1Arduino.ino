5b27898e10b4d461d40fd1592ffd5a15bd7a5ea9
HugoGhesquiere <hugo.ghesquiere@lycee-saintjoseph.eu>
2024-09-30 14:28:12 +0200

    Add files via upload

--- Projet1Arduino.ino
+++ Projet1Arduino.ino
@@ -6,9 +6,10 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   // put your setup code here, to run once:
-  const int fan
-  const int humidite
-  const float temperature
+  const int fan;
+  const int humiditee;
+  const float temperature;
+  Serial.begin(9600);
   lcd.begin(); // Démarre l'afficheur
   lcd.backlight(); // Allume le rétro-éclairage
 
@@ -16,12 +17,25 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if temperature >= AQQCH{
-    digitalWrite(fan, HIGH)
-  };
+  if temperature > 26{
+    digitalWrite(fan, HIGH);
+    Serial.println("Temperature Trop élevée");   // a déplacer ailleur (Ecran lcd ou autre support)
+  }
+  else if temperature < 20{
+     Serial.println("Temperature Trop Basse");   // a déplacer ailleur (Ecran lcd ou autre support)
+  }
   else{
-    digitalWrite(fan, LOW)
-  };
+    digitalWrite(fan, LOW);
+  }
+
+
+  if humiditee > 60{
+    Serial.println("Humiditee Trop Haute");   // a déplacer ailleur (Ecran lcd ou autre support)
+  }
+  else if humiditee < 40{
+    Serial.println("Humiditee Trop Basse");   // a déplacer ailleur (Ecran lcd ou autre support)
+  }
+  
   lcd.print("Hummiditee: ");
   lcd.print(humidite);
   lcd.setCursor(0, 1);
